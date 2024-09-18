dea0b17ae712964e6ba0b9b4adc193ec61366729
Alberto Nunes <bisneto.alberto@gmail.com>
2024-09-18 16:03:20 -0300

    fix FREE_BOOKING_PROCESS_ALIAS

--- src/config/calligoConstants.js
+++ src/config/calligoConstants.js
@@ -1,2 +1,2 @@
 export const FREE_BOOKING_PROCESS_NAME = 'calligo-free-booking';
-export const FREE_BOOKING_PROCESS_ALIAS = `${FREE_BOOKING_PROCESS_NAME}/release-4`;
+export const FREE_BOOKING_PROCESS_ALIAS = `${FREE_BOOKING_PROCESS_NAME}/release`;
